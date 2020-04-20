4af6344928a3d2219a395c20bfb2a0e7a2b7ba20
atusy <30277794+atusy@users.noreply.github.com>
2020-04-20 18:45:58 +0900

    improve class finder

--- R/mini_document.R
+++ R/mini_document.R
@@ -21,19 +21,19 @@ hook_code_class <- function(type, code_folding = c("none", "show", "hide")) {
   summary_type <- paste0("summary.", type)
 
   function(options) {
-    .class <- c(
-      unlist(strsplit(options[[class_type]], " ")),
-      paste0("chunk-", type),
-      details
-    )
+    .class <- c(options[[class_type]], paste0("chunk-", type), details)
     .attr <- options[[attr_type]]
-    if ((code_folding == "show") && (any(c(".hide", "hide") %in% .class))) {
+    if ((code_folding == "show") && any(grepl('(^| )\\.?hide( |$)', .class))) {
       .class <- .class[.class != "show"]
     }
     if ("details" %in% .class) {
       summary <- paste0(
         "summary='",
-        if (is.null(options[[summary_type]])) default_summary[type] else options[[summary_type]],
+        if (is.null(options[[summary_type]])) {
+          default_summary[type]
+        } else {
+          options[[summary_type]]
+        },
         "'"
       )
       .attr <- c(.attr, summary)
