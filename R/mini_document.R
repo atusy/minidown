e7f2c8bf1e30bb6994cc5d48c3bca8643a8e2244
atusy <30277794+atusy@users.noreply.github.com>
2020-04-20 18:20:04 +0900

    run styler::style_pkg()

--- R/mini_document.R
+++ R/mini_document.R
@@ -1,37 +1,37 @@
-default_summary = c(
-  'source' = 'Source', 'output' = 'Output',
-  'message' = 'Message', 'warning' = 'Warning', 'error' = 'Error'
-  )
-
-default_folding <- c(
-  source = 'none',
-  output = 'none',
-  message = 'none',
-  warning = 'none',
-  error = 'none'
+default_summary <- c(
+  "source" = "Source", "output" = "Output",
+  "message" = "Message", "warning" = "Warning", "error" = "Error"
 )
 
-hook_code_class = function(type, code_folding = c('none', 'show', 'hide')) {
+default_folding <- c(
+  source = "none",
+  output = "none",
+  message = "none",
+  warning = "none",
+  error = "none"
+)
+
+hook_code_class <- function(type, code_folding = c("none", "show", "hide")) {
   force(type)
   details <- list(
-      none = NULL, show = c('details', 'show'), hide = 'details'
-    )[[match.arg(code_folding)]]
-  class_type <- paste0('class.', type)
-  attr_type = paste0('attr.', type)
-  summary_type = paste0('summary.', type)
+    none = NULL, show = c("details", "show"), hide = "details"
+  )[[match.arg(code_folding)]]
+  class_type <- paste0("class.", type)
+  attr_type <- paste0("attr.", type)
+  summary_type <- paste0("summary.", type)
 
   function(options) {
     .class <- c(
-      unlist(strsplit(options[[class_type]], ' ')),
-      paste0('chunk-', type),
+      unlist(strsplit(options[[class_type]], " ")),
+      paste0("chunk-", type),
       details
     )
     .attr <- options[[attr_type]]
-    if ((code_folding == 'show') && (any(c('.hide', 'hide') %in% .class))) {
-      .class <- .class[.class != 'show']
+    if ((code_folding == "show") && (any(c(".hide", "hide") %in% .class))) {
+      .class <- .class[.class != "show"]
     }
-    if ('details' %in% .class) {
-      summary = paste0(
+    if ("details" %in% .class) {
+      summary <- paste0(
         "summary='",
         if (is.null(options[[summary_type]])) default_summary[type] else options[[summary_type]],
         "'"
@@ -46,16 +46,16 @@ hook_code_class = function(type, code_folding = c('none', 'show', 'hide')) {
 
 
 
-fold <- function(code_folding = c('none', 'show', 'hide')) {
+fold <- function(code_folding = c("none", "show", "hide")) {
   folding <- default_folding
 
   if (is.character(code_folding)) {
-    folding['source'] <- match.arg(code_folding, c('none', 'show', 'hide'))
+    folding["source"] <- match.arg(code_folding, c("none", "show", "hide"))
     return(folding)
   }
 
   if (is.null(names(code_folding)) || !is.list(code_folding)) {
-    stop('`code_folding` must be a string or a named list.')
+    stop("`code_folding` must be a string or a named list.")
   }
 
 
@@ -71,38 +71,38 @@ fold <- function(code_folding = c('none', 'show', 'hide')) {
 #' @inheritParams rmarkdown::html_document
 #' @export
 mini_document <- function(
-  code_folding = c('none', 'show', 'hide'),
-  pandoc_args = NULL,
-  keep_md = FALSE,
-  includes = list(),
-  ...
-) {
+                          code_folding = c("none", "show", "hide"),
+                          pandoc_args = NULL,
+                          keep_md = FALSE,
+                          includes = list(),
+                          ...) {
   folding <- fold(code_folding)
   dir_template <-
     system.file("rmarkdown", "templates", "mini_document", package = "minidown")
   css_dependency <- htmltools::htmlDependency(
-    'style', '0.0.1', dir_template, stylesheet = 'style.css'
+    "style", "0.0.1", dir_template,
+    stylesheet = "style.css"
   )
 
-  lua <- dir(dir_template, pattern = '\\.lua$', full.names = TRUE)
-  pandoc_args_lua <- c(rbind(rep_len('--lua', length(lua)), lua))
+  lua <- dir(dir_template, pattern = "\\.lua$", full.names = TRUE)
+  pandoc_args_lua <- c(rbind(rep_len("--lua", length(lua)), lua))
   includes$in_header <-
-    c(includes$in_header, file.path(dir_template, 'head.html'))
+    c(includes$in_header, file.path(dir_template, "head.html"))
 
   rmarkdown::output_format(
     knitr = rmarkdown::knitr_options(
-        opts_chunk = list(
-          class.source = '', class.output = '',
-          class.message = '', class.warning = '', class.error = ''
-        ),
-        opts_hooks = list(
-          class.source = hook_code_class('source', folding['source']),
-          class.output = hook_code_class('output', folding['output']),
-          class.message = hook_code_class('message', folding['message']),
-          class.warning = hook_code_class('warning', folding['warning']),
-          class.error = hook_code_class('error', folding['error'])
-        )
+      opts_chunk = list(
+        class.source = "", class.output = "",
+        class.message = "", class.warning = "", class.error = ""
       ),
+      opts_hooks = list(
+        class.source = hook_code_class("source", folding["source"]),
+        class.output = hook_code_class("output", folding["output"]),
+        class.message = hook_code_class("message", folding["message"]),
+        class.warning = hook_code_class("warning", folding["warning"]),
+        class.error = hook_code_class("error", folding["error"])
+      )
+    ),
     pandoc = NULL,
     keep_md = keep_md,
     base_format = rmarkdown::html_document(
