fd15eee9d8d1e13741042561aa1f5066dad2d2d3
atusy <30277794+atusy@users.noreply.github.com>
2020-04-22 18:35:43 +0900

    simplify

--- R/fold.R
+++ R/fold.R
@@ -3,18 +3,19 @@
 #' @inheritParams rmarkdown::html_document
 fold <- function(code_folding = c("none", "show", "hide")) {
   folding <- default_folding
+  .names <- names(code_folding)
 
   if (is.character(code_folding)) {
     folding["source"] <- match.arg(code_folding, c("none", "show", "hide"))
     return(folding)
   }
 
-  if (is.null(names(code_folding)) || !is.list(code_folding)) {
+  if (is.null(.names) || !is.list(code_folding)) {
     stop("`code_folding` must be a string or a named list.")
   }
 
 
-  for (nm in names(code_folding)) {
+  for (nm in .names) {
     folding[nm] <- code_folding[[nm]]
   }
   return(folding)
