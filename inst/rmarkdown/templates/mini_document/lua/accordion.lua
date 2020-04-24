20f4d7c11ecbf99ee1b9a434c428ab42f5c9f281
atusy <30277794+atusy@users.noreply.github.com>
2020-04-24 23:43:10 +0900

    update lua

--- inst/rmarkdown/templates/mini_document/lua/accordion.lua
+++ inst/rmarkdown/templates/mini_document/lua/accordion.lua
@@ -1,7 +1,42 @@
+collapse = 0
+level = 0
+
+function not_collapse(x)
+  return x ~= "collapse"
+end
+
+function Header(elem)
+  if collapse == 1 and elem.level <= level then
+    collapse = 0
+    level = 0
+    return {
+      pandoc.RawBlock("html", "</div>"),
+      elem
+    }
+  elseif elem.classes and elem.classes:find("collapse") then
+    collapse = 1
+    level = elem.level
+    elem.classes = elem.classes:filter(not_collapse)
+    return{
+      elem,
+      pandoc.RawBlock("html", "<div class=collapse>")
+    }
+  end
+end
+
+function Pandoc(doc)
+  if collapse == 1 then
+    table.insert(doc.blocks, pandoc.RawBlock("html", "</div>"))
+    return pandoc.Pandoc(doc.blocks, doc.meta)
+  end
+end
+
+menu = 0
+
 function Div(elem)
   if elem.attr.attributes.menu then
-    local title = elem.attr.attributes.menu
-    local id = title .. '-menu'
+    menu = menu + 1
+    local id = 'accordion-menu-' .. menu
     local checked = ''
     if elem.classes:find('show') then
       checked = 'checked'
@@ -13,7 +48,7 @@ function Div(elem)
         ..
         '<label for="' .. id .. '" aria-hidden="true">'
         ..
-        title
+        elem.attr.attributes.menu
         ..
         '</label>'
       ),
