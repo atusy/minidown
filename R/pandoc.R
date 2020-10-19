31fe30eaeb0d852cedfdc20d026c580e8699c191
atusy <30277794+atusy@users.noreply.github.com>
2020-10-19 09:26:08 +0900

    improve a warning message from check_pandoc_version()

--- R/pandoc.R
+++ R/pandoc.R
@@ -1,7 +1,7 @@
 check_pandoc_version <- function(minimum = "2.0.0", recommend = "2.7.2") {
   if(!rmarkdown::pandoc_available(minimum)) {
     warning("minidown supports Pandoc >=", recommend, ", ",
-            "but system uses Pandoc < 2.0 which is too old.")
+            "but system uses Pandoc < ", minimum, " which is too old.")
     return(FALSE)
   }
 
