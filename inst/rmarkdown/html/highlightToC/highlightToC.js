b865fb93c16c911a5dbb382f8a4bc464338e094c
atusy <30277794+atusy@users.noreply.github.com>
2021-01-09 20:57:29 +0900

    again use scroll event in order to properly highlight the toc

--- inst/rmarkdown/html/highlightToC/highlightToC.js
+++ inst/rmarkdown/html/highlightToC/highlightToC.js
@@ -1,25 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const toc = Array.from(document.querySelectorAll('#TOC li>a')).reduce(
-    (hash, elem) => {
-      hash[elem.hash] = elem.parentNode.classList;
-      return hash;
-    }, {});
-  let previous = null;
+  const anchors = document.querySelectorAll('#TOC li>a');
+  const sections = Array.from(anchors).map(x => document.querySelector(x.hash));
+  const computedStyle = window.getComputedStyle(anchors[0]);
+  const hiColor = computedStyle.backgroundColor.
+    replace(/rgba.*, 0\)/, "white").
+    replace(/(rgba.*), [0-9]+/, "$1, 255");
+  const hiBackgroundColor = computedStyle.color;
+  let highlighted = 0;
 
-  const callback = (entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        const target = toc["#" + entry.target.id];
-        if (previous !== null) {previous.remove("highlight")}
-        target.add("highlight");
-        previous = target;
-      }
-    });
-  };
+  function argMin(x) {
+    return x.indexOf(Math.min(...x));
+  }
 
-  const observer = new IntersectionObserver(
-    callback, {root: null, rootMargin: "0px", threshold: 0}
-  );
+  function highlight() {
+    const y = window.scrollY;
+    console.log(Array.from(sections).map(
+      section => section.getBoundingClientRect().top
+    ));
+    const closest = argMin(Array.from(sections).map(
+      section => Math.pow(section.getBoundingClientRect().top, 2)
+    ));
+    if (highlighted != closest) {
+      anchors[highlighted].style.display = "";
+      anchors[highlighted].style.width = "";
+      anchors[highlighted].style.color = "";
+      anchors[highlighted].style.backgroundColor = "";
+    }
+    anchors[closest].style.display = "inline-block";
+    anchors[closest].style.width = "100%";
+    anchors[closest].style.color = hiColor;
+    anchors[closest].style.backgroundColor = hiBackgroundColor;
+    highlighted = closest;
+  }
 
-  Object.keys(toc).map(x => observer.observe(document.querySelector(x)));
+  highlight();
+  document.addEventListener('scroll', highlight, {passive: true});
 });
