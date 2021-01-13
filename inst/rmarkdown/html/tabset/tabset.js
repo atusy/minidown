b607e147dbb2c7e8ff4e26e4ffa2919dd71d7075
atusy <30277794+atusy@users.noreply.github.com>
2021-01-13 23:43:02 +0900

    implemet UI and navigation

--- inst/rmarkdown/html/tabset/tabset.js
+++ inst/rmarkdown/html/tabset/tabset.js
@@ -1,23 +1,44 @@
 document.addEventListener('DOMContentLoaded', function() {
+  let tabHashes = [];
+
+  function toggle(elem, from = 0, to = 0) {
+    elem[from].classList.remove("active");
+    elem[to].classList.add("active");
+  }
+
+  // Implement
   Array.from(document.querySelectorAll("section.tabset")).forEach(section => {
     const tabs = section.querySelectorAll(":scope>section");
     const tabIds = Array.from(tabs).map(tab => {
-      tab.style.display = tab.children[0].style.display = "none";
+      tab.classList.add("tab");
       return tab.id;
     });
-    let shown = tabs[0];
-    shown.style.display = "";
+    tabHashes.push(...tabIds.map(id => "#" + id));
+    let active = 0;
+    toggle(tabs);
 
     const ul = section.insertBefore(document.createElement("ul"), tabs[0]);
+    ul.classList.add("tabmenu");
     tabIds.forEach(tabId => {
-      const a = ul.appendChild(document.createElement("li")).
-        appendChild(document.createElement("a", {href: "#" + tabId}));
-      a.textContent = tabId;
-      a.addEventListener("click", function() {
-        shown.style.display = "none";
-        shown = tabs[tabIds.indexOf(tabId)];
-        shown.style.display = "";
+      const current = tabIds.indexOf(tabId);
+      tabs[current].removeAttribute("id");
+      const button = ul.appendChild(document.createElement("li")).
+        appendChild(document.createElement("button"));
+      button.id = tabId;
+      button.textContent = tabs[current].children[0].textContent;
+      button.addEventListener("click", function() {
+        toggle(tabs, active, current);
+        toggle(ul.children, active, current);
+        active = current;
       });
     });
+    toggle(ul.children);
+  });
+
+  // Navigation
+  window.addEventListener("hashchange", function() {
+    if (tabHashes.indexOf(location.hash) >= 0) {
+      document.querySelector(location.hash).click();
+    }
   });
 });
