4be83302780bd294a12555ed4268ad9e857ef963
atusy <30277794+atusy@users.noreply.github.com>
2021-01-17 00:13:28 +0900

    update tests

--- tests/testthat/test-frameworks.R
+++ tests/testthat/test-frameworks.R
@@ -1,3 +1,9 @@
 test_that("Returns a html_dependency", {
-  expect_s3_class(html_dependency_framework(), "html_dependency")
+  for (framework in c(names(frameworks), "all")) {
+    lapply(
+      html_dependency_framework(framework),
+      expect_s3_class,
+      "html_dependency"
+    )
+  }
 })
