f25e89e32051449e2e4664c28fcfd6b20f6e06f4
atusy <30277794+atusy@users.noreply.github.com>
2021-04-03 22:37:57 +0900

    update tooltip.lua to suppress error in linux-x86_64-centos-epel

--- inst/rmarkdown/lua/tooltip.lua
+++ inst/rmarkdown/lua/tooltip.lua
@@ -1,11 +1,12 @@
 --wip: remove elem.attr.attributes.tooltip
 
 function Span(elem)
-  local tooltip = elem.attr.attributes.tooltip
+  local tooltip = elem.attributes and elem.attributes.tooltip
   if tooltip then
-    tooltip = '<span class=tooltip aria-label="' .. tooltip .. '">'
     return {
-      pandoc.RawInline("html", tooltip),
+      pandoc.RawInline(
+        "html", '<span class=tooltip aria-label="' .. tooltip .. '">'
+      ),
       elem,
       pandoc.RawInline("html", "</span>")
     }
