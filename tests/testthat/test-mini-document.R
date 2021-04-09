86bdf5628e6d68f62b6bc1f723914243eef432e7
atusy <30277794+atusy@users.noreply.github.com>
2021-04-09 12:46:57 +0900

    fix CRAN policy violation caused by intermediate files from rmarkdown::render (#53)
    * fix CRAN policy violation caused by intermediate files from rmarkdown::render
    
    * update NEWS

--- tests/testthat/test-mini-document.R
+++ tests/testthat/test-mini-document.R
@@ -1,12 +1,14 @@
 testthat::skip_if(!rmarkdown::pandoc_available("2.7.2"))
 
 test_that("render skeleton.Rmd", {
+  temp_rmd <- tempfile(fileext = ".Rmd")
+  file.copy(
+    path_mini_resources("templates", "mini_document", "skeleton",
+                        "skeleton.Rmd"),
+    temp_rmd
+  )
   rendered_file <- rmarkdown::render(
-    path_mini_resources(
-      "templates", "mini_document", "skeleton", "skeleton.Rmd"
-    ),
-    output_file = tempfile(fileext = ".html"),
-    quiet = TRUE
+    temp_rmd, output_file = tempfile(fileext = ".html"), quiet = TRUE
   )
   expect_true(file.exists(rendered_file))
 })
