4b14ed11ad5a220334effec44c29f48a130d5130
atusy <30277794+atusy@users.noreply.github.com>
2021-05-07 23:10:34 +0900

    code_folding hooks by minidown_hook chunk option instead of source (#57)
    * use trigger a hook with  minidown_hook chunk option instead of source
    
    * update NEWS

--- R/spec-knitr-opts-hooks.R
+++ R/spec-knitr-opts-hooks.R
@@ -1,3 +1,5 @@
 spec_opts_hooks <- function(code_folding) {
-  list(engine = gen_hook_code_folding(code_folding_options(code_folding)))
+  list(
+    minidown_hook = gen_hook_code_folding(code_folding_options(code_folding))
+  )
 }
